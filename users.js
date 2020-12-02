afe65e173cc991dfd706bb61d6340b151c8e379a
subhankr12 <subhankr12@gmail.com>
2020-12-02 12:05:48 +0530

    create other functions for users

--- users.js
+++ users.js
@@ -16,14 +16,21 @@ const users = [];
     return { user };
  }
 
- const removeUser = () => {
+ const removeUser = (id) => {
+    const index = users.findIndex(user => user.id === id);
 
+    if(index !== -1){
+        return users.splice(index, 1)[0];
+    }
  }
 
- const getUser = () => {
+ const getUser = (id) => users.find(user => user.id === id);
 
- }
+ const getUsersInRoom = (room) => users.filter(user => user.room === room);
 
- const getUsersInRoom = () => {
-
+ module.exports = {
+     addUser,
+     removeUser,
+     getUser,
+     getUsersInRoom,
  }
