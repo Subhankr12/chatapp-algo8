da4a07141c1ade62250c0992d8e73bb63bf8175c
subhankr12 <subhankr12@gmail.com>
2020-12-02 12:35:22 +0530

    start sending messages at client side

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -7,6 +7,8 @@ let socket;
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
@@ -18,7 +20,7 @@ const Chat = ({ location }) => {
         setRoom(room);
 
         socket.emit('join', { name, room }, () => {
-         
+            
         });
 
         return () => {
@@ -27,8 +29,29 @@ const Chat = ({ location }) => {
             socket.off();
         }
     }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if(message){
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages);
+
     return (
-        <h1>Chat</h1>
+        <div className="container">
+            <div className="wrapper">
+                <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null} />
+            </div>
+        </div>
     )
 }
 
