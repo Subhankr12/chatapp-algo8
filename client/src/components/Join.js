102132b17f8650a2def40ce95e971d9bc7053aca
subhankr12 <subhankr12@gmail.com>
2020-12-02 11:43:04 +0530

    solve cors issue and create join and chat

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <h1>Join</h1>
+        <div className="container">
+            <div className="wrapper">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joininput" type="text" onChange={(e) => setName(e.target.value)} /></div>
+                <div><input placeholder="Room" className="joininput mt-20" type="text" onChange={(e) => setRoom(e.target.value)} /></div>
+                <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="btn mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+      }
+});
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
