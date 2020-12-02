78b6eb5e40ba7fe1486b9798334b5ee4e9e9691b
subhankr12 <subhankr12@gmail.com>
2020-12-02 14:36:17 +0530

    remove user when disconnected and get all users in a room

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+
+
         callback();
     });
 
@@ -38,12 +41,17 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
 
         io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, text: getUsersInRoom(user.room)});
 
         callback();
     })
 
     socket.on('disconnect', () => {
-        console.log('User had left!!!');
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left` });
+        }
     })
 })
 
